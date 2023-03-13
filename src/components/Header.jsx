7787d23f1128cebbdec23e893c88be70340b330b
jeferson-viana <jefersonv28@gmail.com>
2023-03-13 18:23:01 -0300

    Requisito 4 concluído

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
       <header data-testid="header-component">
-        Teste
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Music favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
