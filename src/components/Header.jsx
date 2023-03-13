26e454e1b13986c4f39a204663644a2c47048ca3
jeferson-viana <jefersonv28@gmail.com>
2023-03-13 18:13:40 -0300

    Requisito 3 concluído

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header data-testid="header-component">
+        Teste
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      isTrue: false,
+    };
+  }
+
+  componentDidMount() {
+    this.teste();
+  }
+
+  teste = async () => {
+    const response = await getUser();
+    this.setState({
+      name: response.name,
+      isTrue: true,
+    });
+  };
+
   render() {
+    const { name, isTrue } = this.state;
+    const welcome = <h1 data-testid="header-user-name">{ `Welcome, ${name}` }</h1>;
     return (
-      <div data-testid="page-search">Search</div>
+      <div data-testid="page-search">
+        <Header />
+        { isTrue ? welcome : <Carregando />}
+      </div>
     );
   }
 }
