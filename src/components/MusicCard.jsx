efdb74c1a8d43111690081ebea4238eda2e3fe18
jeferson-viana <jefersonv28@gmail.com>
2023-03-14 18:26:24 -0300

    Requisito 8 concluído

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Carregando from '../pages/Carregando';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isTrue: true,
+      validation: false,
+      array: [],
+    };
+  }
+
+  handleChecked = async () => {
+    const { array } = this.state;
+    this.setState({
+      isTrue: false,
+    });
+    const { music } = this.props;
+    const response = await addSong(music);
+    this.setState({
+      isTrue: true,
+      validation: true,
+      array: [...array, response],
+    });
+  };
+
   render() {
     const { music } = this.props;
+    const { isTrue, validation } = this.state;
     return (
-      <li>
-        <audio data-testid="audio-component" src={ music.previewUrl } controls>
-          <track kind="captions" />
-          <code>audio</code>
-        </audio>
-      </li>
+      isTrue ? (
+        <li>
+          <audio data-testid="audio-component" src={ music.previewUrl } controls>
+            <track kind="captions" />
+            <code>audio</code>
+          </audio>
+          <label data-testid={ `checkbox-music-${music.trackId}` }>
+            Favorita
+            <input
+              type="checkbox"
+              defaultChecked={ validation }
+              onClick={ this.handleChecked }
+            />
+          </label>
+        </li>
+      ) : <Carregando />
     );
   }
 }
@@ -18,6 +55,7 @@ class MusicCard extends React.Component {
 MusicCard.propTypes = {
   music: PropTypes.shape({
     previewUrl: PropTypes.string.isRequired,
+    trackId: PropTypes.number.isRequired,
   }).isRequired,
 };
 
